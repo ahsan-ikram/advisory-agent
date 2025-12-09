9730c38abe006da9fa98d78174043b01aabdb13b
ahsan <muhammad.ahsan@gmail.com>
2025-12-09 09:57:36 +0100

    Code refactor

--- src/advisor/agent.py
+++ src/advisor/agent.py
@@ -1,11 +1,15 @@
 from google.adk.agents.llm_agent import Agent
-
+from pathlib import Path
+from advisor.tools import get_service_offerings
+from advisor.tools import get_professional_experience
 from helper import read_prompt
 
 
+agent_name = "advisor"
 root_agent = Agent(
+    name=agent_name,
     model="gemini-2.5-flash",
-    name="root_agent",
-    description="A helpful assistant for user questions.",
-    instruction= read_prompt("system"),
+    description=read_prompt(agent_name, "purpose"),
+    instruction=read_prompt(agent_name, "system"),
+    tools=[get_service_offerings, get_professional_experience],
 )
